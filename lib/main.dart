ebc76754e1b4c8c9e26efb48481d411c899ef7ee
Kimi Liang <kimi.liang@accentrix.com>
2019-11-06 11:20:05 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-
-void main() {
-  runApp(MyApp());
-}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Cached Images';
-
     return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: CachedNetworkImage(
-            placeholder: (context, url) => CircularProgressIndicator(),
-            imageUrl:
-              'https://picsum.photos/250?image=9',
+      title: 'Animate',
+      home: Container(
+        child: Scaffold(
+          body: Center(
+            child: AnimateObj(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class AnimateObj extends StatefulWidget {
+  AnimateObj({Key key}) : super(key: key);
+
+  _AnimateObjState createState() => _AnimateObjState();
+}
+
+class _AnimateObjState extends State<AnimateObj> {
+  AnimationController _wController;
+  AnimationController _hController;
+  AnimationController _colorController;
+  Tween<double> _w;
+  Tween<double> _h;
+  Tween<Color> _color;
+
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
     );
   }
 }
